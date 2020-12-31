ca29e8937c75b78bcc9b12b7b304828741457b30
Pablo Bondia-Luttiau <pablobondialuttiau@gmail.com>
2020-12-31 15:31:59 +0100

    Added lazy loading

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,39 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/Home.vue'
-import Items from "@/views/Items";
-import Moves from "@/views/Moves";
-import Pokemons from "@/views/Pokemons";
+
+const Home = () => import('@/views/Home')
+const Items = () => import('@/views/Items')
+const Moves = () => import('@/views/Moves')
+const Pokemons = () => import('@/views/Pokemons')
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Accueil',
-    component: Home
-  },
-  {
-    path: '/items',
-    name: 'Objets',
-    component: Items
-  },
-  {
-    path: '/moves',
-    name: 'Capacités',
-    component: Moves
-  },
-  {
-    path: '/pokemons',
-    name: 'Pokémons',
-    component: Pokemons
-  },
+    {
+        path: '/',
+        name: 'Accueil',
+        component: Home
+    },
+    {
+        path: '/items',
+        name: 'Objets',
+        component: Items
+    },
+    {
+        path: '/moves',
+        name: 'Capacités',
+        component: Moves
+    },
+    {
+        path: '/pokemons',
+        name: 'Pokémons',
+        component: Pokemons
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
